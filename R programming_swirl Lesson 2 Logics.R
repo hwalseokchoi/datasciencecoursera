bf8b4821b162a9a9b2d9429a6b18f7ae412b06e3
hwalseokchoi <hwals.choi7234@gmail.com>
2021-07-21 08:39:25 +0900

    R programming swirl Lesson2: Functions

---
+++ R programming_swirl Lesson 2 Logics.R
@@ -0,0 +1,35 @@
+swirl()
+library(swirl)
+TRUE == TRUE
+F
+(FALSE == TRUE) == FALSE
+6 == 7
+6 < 7
+10 <= 10
+5 != 7
+5 = !7
+5 == !7
+!5 == 7
+FALSE & FALSE
+TRUE & c(TRUE, FALSE, FALSE)
+c(TRUE, TRUE, TRUE) & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+TRUE | c(TRUE, FALSE, FALSE)
+TRUE || c(TRUE, FALSE, FALSE)
+5 > 8 || 6 != 8 && 4 > 3.9
+isTRUE(6 > 4)
+2
+identical("twins", "twins")
+3
+xor(5 == 6, !FALSE)
+1
+ints <- sample(10)
+ints
+ints > 5
+which(ints > 7)
+whichi(ints <= 2)
+1
+any(ints < 0)
+all(ints > 0)
+2
+2
